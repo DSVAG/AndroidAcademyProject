7f4de50b143435f11d9b7160549960a30fef21ab
Vitaly Pestryakov <xm1t1mx@gmail.com>
2020-11-08 13:48:31 +0300

    commit init

--- app/src/main/java/com/dsvag/androidacademyproject/MainActivity.kt
+++ app/src/main/java/com/dsvag/androidacademyproject/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.dsvag.androidacademyproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dsvag.androidacademyproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/dsvag/androidacademyproject/ViewBinding.kt
@@ -0,0 +1,9 @@
+package com.dsvag.androidacademyproject
+
+import android.app.Activity
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+
+inline fun <V : ViewBinding> Activity.viewBinding(crossinline inflate: (LayoutInflater) -> V): Lazy<V> {
+    return lazy(LazyThreadSafetyMode.NONE) { inflate(layoutInflater) }
+}
